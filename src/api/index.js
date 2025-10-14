88810d1adcfea67682fe628ca6d6dd5a4435624d
AGS3 <moorthy.chandiran21@gmail.com>
2025-10-14 15:36:38 +0530

    commit -1

--- src/api/index.js
+++ src/api/index.js
@@ -22,7 +22,6 @@ export const CHAPTER_DATASOURCE_CREATE = `${BASE_URL}/api/create-datasources`;
 export const CHAPTER_DATASOURCE_UPDATE_BY_ID = `${BASE_URL}/api/update-datasources`;
 
 //STATES
-
 export const STATES_LIST = `${BASE_URL}/api/fetch-states`;
 export const CREATE_STATES = `${BASE_URL}/api/create-states`;
 export const UPDATES_STATES = `${BASE_URL}/api/update-states`;
