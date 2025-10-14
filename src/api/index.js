b5d8b300730787d5728ffbf7936d410f605400cb
AGS3 <moorthy.chandiran21@gmail.com>
2025-10-14 15:42:10 +0530

    Revert "commit -1"
    This reverts commit d0cecf4ce84a2369ce1f10bfc87468252d2d7b29.

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import BASE_URL from "@/config/base-url";
 
 import Cookies from "js-cookie";
 import { decryptId, encryptId } from "@/utils/encyrption/encyrption";
+
 /*--------------------------Master-start----------------------------------- */
 // CHAPTER
 export const CHAPTER_LIST = `${BASE_URL}/api/fetch-chapters`;
@@ -11,7 +12,7 @@ export const ADD_CHAPTER_SUMBIT = `${BASE_URL}/api/chapter`;
 export const CHAPTER_VIEW_BY_ID = `${BASE_URL}/api/fetch-chapter-by-id`;
 export const CHAPTER_VIEW_CREATE_USER = `${BASE_URL}/api/create-user`;
 export const CHAPTER_VIEW_UPDATE_USER = `${BASE_URL}/api/update-user`;
-//(Edit)
+//(edit)
 export const CHAPTER_EDIT_BY_ID = `${BASE_URL}/api/fetch-chapter-by-id`;
 export const CHAPTER_EDIT_BY_ID_UPDATE = `${BASE_URL}/api/update-chapter`;
 export const CHAPTER_EDIT_STATES_DROPDOWN = `${BASE_URL}/api/fetch-states`;
@@ -21,6 +22,7 @@ export const CHAPTER_DATASOURCE_CREATE = `${BASE_URL}/api/create-datasources`;
 export const CHAPTER_DATASOURCE_UPDATE_BY_ID = `${BASE_URL}/api/update-datasources`;
 
 //STATES
+
 export const STATES_LIST = `${BASE_URL}/api/fetch-states`;
 export const CREATE_STATES = `${BASE_URL}/api/create-states`;
 export const UPDATES_STATES = `${BASE_URL}/api/update-states`;
